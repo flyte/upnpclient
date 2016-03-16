b82ae21ed7453049ff426e11263763151fc85247
Ferry Boender <ferry.boender@gmail.com>
2016-03-16 09:03:56 +0100

    Show a bit more info in discover example

--- examples/discover.py
+++ examples/discover.py
@@ -10,4 +10,6 @@ ssdp = upnpclient.SSDP(wait_time=5)
 servers = ssdp.discover()
 
 for server in servers:
-    print server.friendly_name
+    print server.friendly_name, '@', server.location
+    for service in server.services:
+        print "   ", service.service_id, service.service_type
